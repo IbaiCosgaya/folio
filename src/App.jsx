f334f8e644e15fafaba45c00c54f9acba0f90a96
IbaiCosgaya <koskiman19@gmail.com>
2026-05-27 17:20:52 +0200

    estructura de pantallas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+
 function App() {
   return (
-    <div>
-      <h1>Folio</h1>
-      <p>Tu diario de lectura</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div>
+      <h1>Folio</h1>
+      <p>Inicia sesión para continuar</p>
+    </div>
+  )
+}
+
+export default Login
